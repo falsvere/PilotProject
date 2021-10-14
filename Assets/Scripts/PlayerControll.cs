3d842b86f23f242c7913240365f83101f40139ca
falsvere <tvoyo24@gmail.com>
2021-10-14 22:59:11 +0300

    add dirty move mechanic

--- Assets/Scripts/PlayerControll.cs
+++ Assets/Scripts/PlayerControll.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class PlayerControll : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D playerRB;
+
+
+    private float speed = 12;
+    private float rotation = 60;
+
+    private bool isOnFloor = false;
+
     void Start()
     {
-        sss
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    private void FixedUpdate()
+    {
+
+ 
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if(horizontalInput != 0)
+        {
+
+            Vector2 moveDirection = new Vector2(speed, 0) * horizontalInput;
+
+            playerRB.velocity  = moveDirection;
+            playerRB.MoveRotation(playerRB.rotation * rotation * Time.fixedDeltaTime);
+        }
+        else
+        {
+            if (isOnFloor)
+            {
+                playerRB.velocity *= 0f;
+                playerRB.rotation *= 0f;
+            }
+
+        }
+
+
+    }
+
     void Update()
     {
-        
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.name == "Floor")
+        {
+            isOnFloor = true;
+        }
     }
 }
