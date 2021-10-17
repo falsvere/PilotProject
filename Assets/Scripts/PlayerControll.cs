710abc8276a3e55c170696a863b0c742d676e898
falsvere <tvoyo24@gmail.com>
2021-10-17 17:51:04 +0300

    add jump mechanic, polish movemment

--- Assets/Scripts/PlayerControll.cs
+++ Assets/Scripts/PlayerControll.cs
@@ -6,10 +6,17 @@ public class PlayerControll : MonoBehaviour
 {
     private Rigidbody2D playerRB;
 
-
+    [SerializeField]
     private float speed = 12;
-    private float rotation = 60;
+    [SerializeField]
+    private float speedInJump = 20;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private float rotation = 400;
 
+
+    private bool activateJump = false;
     private bool isOnFloor = false;
 
     void Start()
@@ -19,37 +26,58 @@ public class PlayerControll : MonoBehaviour
 
     private void FixedUpdate()
     {
-
- 
-
+        MoveFunction();
+    }
+    
+    private void MoveFunction()
+    {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        if(horizontalInput != 0)
+
+        Debug.Log(playerRB.velocity.x);
+        if (activateJump && horizontalInput == 0)
         {
+            isOnFloor = false;
+            playerRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            activateJump = false;
 
-            Vector2 moveDirection = new Vector2(speed, 0) * horizontalInput;
+        } else if (horizontalInput != 0 && activateJump)
+        {
+            isOnFloor = false;
+            playerRB.AddForce(new Vector2(speedInJump * horizontalInput, jumpForce), ForceMode2D.Impulse);
+            activateJump = false;
 
-            playerRB.velocity  = moveDirection;
-            playerRB.MoveRotation(playerRB.rotation * rotation * Time.fixedDeltaTime);
-        }
-        else
+        }else if (horizontalInput != 0 && !activateJump)
         {
             if (isOnFloor)
             {
-                playerRB.velocity *= 0f;
-                playerRB.rotation *= 0f;
-            }
+                Vector2 moveDirection = new Vector2(speed, 0) * horizontalInput;
+                float RBRotation = playerRB.rotation + rotation * Time.fixedDeltaTime * - horizontalInput;
 
+                playerRB.velocity = moveDirection;
+      
+                playerRB.MoveRotation(RBRotation);
+            }
+            else
+            {
+                playerRB.AddForce(new Vector2(speedInJump * horizontalInput, 0), ForceMode2D.Force);
+            }
         }
 
-
+        if (isOnFloor && horizontalInput == 0)
+        {
+            playerRB.velocity *= 0f;
+        }
     }
 
-    void Update()
+    private void Update()
     {
-
-
+        if (Input.GetButtonDown("Jump") && isOnFloor)
+        {
+            activateJump = true;
+        }
     }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -57,5 +85,12 @@ public class PlayerControll : MonoBehaviour
         {
             isOnFloor = true;
         }
+
+
+        if (collision.collider.name == "Border")
+        {
+            playerRB.velocity *= 0f;
+        }
     }
+
 }
