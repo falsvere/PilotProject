bd20b002c7d2ba1dcf9b449f0f000b0737e43b99
falsvere <tvoyo24@gmail.com>
2021-11-20 21:34:26 +0300

    add some folders for scripts and refac desttroy platform coroutine

--- Assets/Scripts/PlatformControll.cs
+++ Assets/Scripts/PlatformControll.cs
@@ -9,26 +9,36 @@ public class PlatformControll : MonoBehaviour
 
     private GameManager gameManager;
 
+    private bool isCoroutineActive = false;
+
 
     void Start()
     {
-        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        gameManager = FindObjectOfType<GameManager>();
     }
 
     private IEnumerator DestroyPlatformCoroutine()
     {
+        isCoroutineActive = true;
+
         for(int i = 0; i <= timeBeforeDestroy; i++) {
             yield return new WaitForSeconds(1);
             Debug.Log("Time before destroy :" + (timeBeforeDestroy - i));
         }
-        gameManager.ActivateChangePlatformStateCoroutune(gameObject);
+
+        gameObject.SetActive(false);
+        yield return new WaitForSeconds(5);
+        Debug.Log("should activate");
+        gameObject.SetActive(true);
+
+        isCoroutineActive = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.collider.name == "Player")
-        { 
-           StartCoroutine(DestroyPlatformCoroutine());
+        if(collision.collider.name == "Player" && !isCoroutineActive)
+        {
+            gameManager.StartCoroutine(DestroyPlatformCoroutine());
         }
     }
 }
--- Assets/Scripts/PlayerControll.cs
+++ Assets/Scripts/PlayerControll.cs
@@ -104,5 +104,4 @@ public class PlayerControll : MonoBehaviour
             isOnFloor = false;
         }
     }
-
 }
