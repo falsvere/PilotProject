e5b5e933022b226ec4e4292efae37dfa88788901
falsvere <tvoyo24@gmail.com>
2021-11-21 10:57:38 +0300

    platform inheritance done

--- Assets/Scripts/BaseClasses/BasePlatform.cs
+++ Assets/Scripts/BaseClasses/BasePlatform.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public abstract class BasePlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private GameManager gameManager;
+    private bool isCoroutineActive = false;
+
+    public void SetGameManager()
     {
-        
+        gameManager = FindObjectOfType<GameManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    //корутина исчесзновения и появления платформы
+    public virtual IEnumerator DestroyPlatformCoroutine(int timeBeforeDestroy, int timeBeforeAppear)
     {
-        
+        isCoroutineActive = true;
+
+        for (int i = 0; i <= timeBeforeDestroy; i++)
+        {
+            yield return new WaitForSeconds(1);
+            Debug.Log("Time before destroy :" + (timeBeforeDestroy - i));
+        }
+
+        gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(timeBeforeAppear);
+
+        Debug.Log("should activate");
+        gameObject.SetActive(true);
+
+        isCoroutineActive = false;
+    }
+
+    // если игрок коснулся платформы и корутина изменения еще не запущена - запускаем ее на гейм менеджере тк при отключении объекта корутина остановится
+    public virtual void RunPlatformStateCoroutine(Collision2D collision, int timeBeforeDestroy, int timeBeforeAppear)
+    {
+        if (collision.collider.name == "Player" && !isCoroutineActive)
+        {
+            gameManager.StartCoroutine(DestroyPlatformCoroutine(timeBeforeDestroy, timeBeforeAppear));
+        }
     }
 }
