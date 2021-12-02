f7435dc22c4326ee4b5ed2a98419ad71d3467eff
falsvere <tvoyo24@gmail.com>
2021-12-02 21:47:39 +0300

    delete extra border-coorinates dictioonary

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -5,19 +5,6 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private readonly Dictionary<char, float> gameBorders = new Dictionary<char, float> {
-        {'x', 30f },
-        {'y', 15f },
-    }; 
-
-    public  Dictionary<char, float> gameBordersPublic
-    {
-        get
-        {
-            return gameBorders;
-        }
-    }
-
     void Start()
     {
         
