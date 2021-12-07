2dcb9cb16c22242df489b0583f74cb8e370ee472
falsvere <tvoyo24@gmail.com>
2021-12-07 22:18:19 +0300

    trying to fix attack preparation for circle

--- Assets/Scripts/Enemies/AICircleEnemy.cs
+++ Assets/Scripts/Enemies/AICircleEnemy.cs
@@ -11,11 +11,18 @@ public class AICircleEnemy : MonoBehaviour
     private bool isPlayerDontMoveActive = false;
     private Coroutine playerDontMove;
 
-    private Vector3 playerPreviousPosition;
+    private Vector3 playerPreviousPosition = new Vector3(0,0,-999);
 
     //mehods
     void Start()
     {
+        Debug.Log(playerDontMove);
+        playerDontMove = StartCoroutine(OneSecDelayBeforeAttackPrep());
+        Debug.Log(playerDontMove);
+        StopCoroutine(playerDontMove);
+        Debug.Log(playerDontMove);
+
+
         player = GameObject.FindGameObjectWithTag("Player");
         playerPreviousPosition = player.transform.position;
         cirlceControll = gameObject.GetComponent<CircleEnemyControll>();
@@ -45,39 +52,60 @@ public class AICircleEnemy : MonoBehaviour
 
     private IEnumerator OneSecDelayBeforeAttackPrep()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(5);
+        /*        Debug.Log("Corouutinestrrt");
 
-        Debug.Log("Attack preparation");
+
+
+                Debug.Log("Attack preparation");
+
+                isPlayerDontMoveActive = false;*/
     }
 
     private void FixedUpdate()
     {
         float distanceToPlayer = Vector2.Distance(player.transform.position, transform.position);
 
-        if (distanceToPlayer <= attackRange )
+/*        if (distanceToPlayer <= attackRange )
         {
-            Vector3 playerNewPosition = player.transform.position;
-            float xDifference = Mathf.Max(playerPreviousPosition.x, playerNewPosition.x) - Mathf.Min(playerPreviousPosition.x, playerNewPosition.x);
-            float yDifference = Mathf.Max(playerPreviousPosition.y, playerNewPosition.y) - Mathf.Min(playerPreviousPosition.y, playerNewPosition.y);
+            bool isPlayerMoveAlot = true;
+            if (playerPreviousPosition.z == -999)
+            {
+                playerPreviousPosition = player.transform.position;
+            } else
+            {
+                Vector3 playerNewPosition = player.transform.position;
+                float xDifference = Mathf.Max(playerPreviousPosition.x, playerNewPosition.x) - Mathf.Min(playerPreviousPosition.x, playerNewPosition.x);
+                float yDifference = Mathf.Max(playerPreviousPosition.y, playerNewPosition.y) - Mathf.Min(playerPreviousPosition.y, playerNewPosition.y);
 
-            bool isPlayerMoveAlot = xDifference >= 1 || yDifference >= 1;
-            playerPreviousPosition = playerNewPosition;
+                isPlayerMoveAlot = xDifference >= 1 || yDifference >= 1;
+                playerPreviousPosition = playerNewPosition;
+            }
+            Debug.Log(isPlayerMoveAlot);
 
             if (isPlayerMoveAlot) {
                 if(isPlayerDontMoveActive)
                 {
                     StopCoroutine(playerDontMove);
+                    Debug.Log(playerDontMove);
+                    isPlayerDontMoveActive = false;
                 }
             } else {
+                if (!isPlayerDontMoveActive)
+                {
                     playerDontMove = StartCoroutine(OneSecDelayBeforeAttackPrep());
+                    isPlayerDontMoveActive = true;
+                }
             }
 
         } else
         {
+            playerPreviousPosition.z = -999;
             if (isPlayerDontMoveActive)
             {
                 StopCoroutine(playerDontMove);
+                isPlayerDontMoveActive = false;
             }
-        }
+        }*/
     }
 }
